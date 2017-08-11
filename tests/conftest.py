a82d596421d0885cd4a7e11ae32648ba9bd2cdf2
Victor Gavro <vgavro@gmail.com>
2017-08-11 23:05:16 +0300

    added auth tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
 import pytest
 from selenium import webdriver
 
+from tests.test_auth import login
+
 
 @pytest.fixture(scope='session')
+def auth_driver(variables):
+    driver = webdriver.Chrome()
+    login(driver, variables)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
 def driver():
-    return webdriver.Chrome()
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,2 +1,35 @@
-def test_auth_success(driver):
-    pass
+def login(driver, variables):
+    driver.get(variables['url'])
+    el = driver.find_element_by_css_selector('input[type="email"][formcontrolname="username"]')
+    el.send_keys(variables['auth']['login'])
+
+    el = driver.find_element_by_css_selector('input[type="password"][formcontrolname="password"]')
+    el.send_keys(variables['auth']['password'])
+
+    el = driver.find_element_by_css_selector('button[type="submit"]')
+    assert el.text == 'Login'
+    el.click()
+
+    assert driver.find_element_by_css_selector('li.dv-user-profile')
+
+
+def test_auth_success(driver, variables):
+    login(driver, variables)
+
+
+def test_auth_failed(driver, variables):
+    driver.get(variables['url'])
+    el = driver.find_element_by_css_selector('input[type="email"][formcontrolname="username"]')
+    el.send_keys('dsfsdf')
+
+    el = driver.find_element_by_css_selector('input[type="password"][formcontrolname="password"]')
+    el.send_keys(variables['auth']['password'])
+
+    el = driver.find_element_by_css_selector('button[type="submit"]')
+    assert el.text == 'Login'
+    el.click()
+
+    if driver.find_elements_by_css_selector('li.dv-user-profile'):
+        raise AssertionError()
+    el = driver.find_element_by_css_selector('p.help-block')
+    assert el.text == 'Username must be a valid email address'
